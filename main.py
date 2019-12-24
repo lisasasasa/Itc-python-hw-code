abb23c5aaf6e0b0d68ce746b10c9fe44514a6545
baluteshih <brain369999@gmail.com>
2019-12-24 18:29:27 +0800

    modified main.py, testing version1

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ if __name__ == '__main__':
     args = get_args() 
     crawler = Crawler()
     contents = crawler.crawl(args.start_date, args.end_date)
-    # TODO: write content to file according to spec
-   # with open(args.py) as args:
+    # TODO: write content to file according to spec 
+    f = open(args.output+'.csv','w')
+    f.write('Post date,Title,Content\n')
+    for data,title,content in contents:
+       f.write('"'+data.replace('"','""')+'",') 
+       f.write('"'+title.replace('"','""')+'",') 
+       f.write('"'+content.replace('"','""')+'"\n') 
+    f.close()
 
